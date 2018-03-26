b8cd723b413ef9195d902173e9166a727e6a867e
Tobias Brandt <tobias@argonasset.co.za>
2018-03-26 11:26:26 +0200

    Refactors the contextmanager method into a class

--- pydiverse/contexts.py
+++ pydiverse/contexts.py
@@ -1,38 +1,43 @@
 from __future__ import print_function
-import contextlib
 
 
+class BoundObjAndStoredGlobals(object):
 
-@contextlib.contextmanager
-def bound_obj_and_stored_globals(obj, globals_):
+    def __init__(self, obj, globals_):
+        self.obj = obj
+        self.globals_ = globals_
 
+    @staticmethod
     def safe_keys(keys, exclusions=['In', 'Out']):
-        return [k for k in keys if not k.startswith('_') and k not in exclusions]
+        return [k for k in keys if not k.startswith('_') and k not in
+                exclusions]
 
-    # store existing global variables
-    stored_globals = {}
-    for k in safe_keys(globals_):
-        stored_globals[k] = globals_[k]
-        del globals_[k]
-    # import the variables from obj
-    for k in obj:
-        globals_[k]= obj[k]
-    yield stored_globals
-    # update obj and remove the temporary global variables
-    for k in safe_keys(globals_):
-        obj[k] = globals_[k]
-        del globals_[k]
-    # restore the stored global variables
-    for k in stored_globals:
-        globals_[k] = stored_globals[k]
-    return
+    def __enter__(self):
+        # store existing global variables
+        self.stored_globals = {}
+        for k in self.safe_keys(self.globals_):
+            self.stored_globals[k] = self.globals_[k]
+            del self.globals_[k]
+        # import the variables from obj
+        for k in self.obj:
+            self.globals_[k]= self.obj[k]
+        return self.stored_globals
 
+    def __exit__(self, *args):
+        # update obj and remove the temporary global variables
+        for k in self.safe_keys(self.globals_):
+            self.obj[k] = self.globals_[k]
+            del self.globals_[k]
+        # restore the stored global variables
+        for k in self.stored_globals:
+            self.globals_[k] = self.stored_globals[k]
+        return
 
 if __name__=='__main__':
     d = dict(a=1, b=2, c=3)
     print(d)
     print([k for k in globals().keys() if not k.startswith('_')])
-    with bound_obj_and_stored_globals(d, globals()):
+    with BoundObjAndStoredGlobals(d, globals()):
         # print(globals().keys())
         # print([k for k in globals().keys() if not k.startswith('_')])
         d = 4
